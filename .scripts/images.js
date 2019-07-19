8cbc7b437c851712569d83981429a96cfe9a8210
Jere Menichelli <jmenichelli@gmail.com>
2019-07-19 13:35:55 +0200

    refactor: add try/catch to images script

--- .scripts/images.js
+++ .scripts/images.js
@@ -7,17 +7,22 @@ const config = require('./config.json')
 
 async function main() {
   console.log(`processing ${chalk.blue('images')}\n`)
-  await asyncMakeDirectory('./assets/images')
-  config.images.map(async (file) => {
-    // read image file with jimp
-    const image = await jimp.read(file.entry)
 
-    // process and write image file to assets
-    image.quality(file.quality)
-    image.resize(file.resize[0], file.resize[1])
-    image.write(file.output)
-    console.log(`${chalk.green(file.output)} image processed\n`)
-  })
+  try {
+    await asyncMakeDirectory('./assets/images')
+    config.images.map(async (file) => {
+      // read image file with jimp
+      const image = await jimp.read(file.entry)
+
+      // process and write image file to assets
+      image.quality(file.quality)
+      image.resize(file.resize[0], file.resize[1])
+      image.write(file.output)
+      console.log(`${chalk.green(file.output)} image processed\n`)
+    })
+  } catch (error) {
+    console.log(chalk.red(error))
+  }
 }
 
 main()
