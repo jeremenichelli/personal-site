fccb99267d9a040334358b347695e8bcb8e9867f
Jere Menichelli <jmenichelli@gmail.com>
2020-07-05 12:02:52 +0200

    fix: move codepen script to single defer load in head

--- .eleventy/shortcodes.js
+++ .eleventy/shortcodes.js
@@ -2,23 +2,18 @@ const deindent = (str) => str.replace(/\n|\t/g, '').trim()
 
 const codeExampleLink = (link) =>
   deindent(`
-  <p class="code-example__link">
-    <a href=${link} rel="noopener noreferrer" target="_blank">
-      See this example in action
-    </a>
-  </p>
+    <p class="code-example__link">
+      <a href=${link} rel="noopener noreferrer" target="_blank">
+        See this example in action
+      </a>
+    </p>
 `)
 
 const codepen = (hash) =>
   deindent(`
-  <p data-height="300" data-theme-id="1" data-slug-hash="${hash}" data-default-tab="html,result" data-user="jeremenichelli" data-embed-version="2" class="codepen">
-    <p class="code-example__link">
-      <a href="https://codepen.io/jeremenichelli/pen/${hash}/" rel="noopener noreferrer" target="_blank">
-        See this example in CodePen
-      </a>
-    </p>
-  </p>
-  <script defer src="https://static.codepen.io/assets/embed/ei.js"></script>
+    <div data-height="300" data-theme-id="1" data-slug-hash="${hash}" data-default-tab="html,result" data-user="jeremenichelli" data-embed-version="2" class="codepen">
+      ${codeExampleLink(`https://codepen.io/jeremenichelli/pen/${hash}/`)}
+    </div>
 `)
 
 module.exports = {
