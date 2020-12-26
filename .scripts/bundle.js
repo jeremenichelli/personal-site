c699ac45dfa79ac8b76cd6a0f428ed0ffaf95a83
Jere Menichelli <jmenichelli@gmail.com>
2020-12-26 10:34:57 +0100

    chore(Build): Revert iife change

--- .scripts/bundle.js
+++ .scripts/bundle.js
@@ -54,7 +54,7 @@ async function main(env = ENVIRONMENT) {
   )
 
   try {
-    const format = 'es'
+    const format = 'iife'
     const sourcemap = env === 'development' ? 'inline' : false
 
     for (const bundle of bundles) {
