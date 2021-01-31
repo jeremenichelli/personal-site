47fe31fee65d242003f7515a6fb50c1831074cd2
Jere Menichelli <jmenichelli@gmail.com>
2021-01-31 19:19:41 +0100

    chore(Performance): Avoid preload tags in favor of JavaScript strategy

--- src/js/main.js
+++ src/js/main.js
@@ -48,3 +48,47 @@ colorSchemeButton.addEventListener('click', function () {
       : COLOR_SCHEME_LIGHT_VALUE
   )
 })
+
+// Preload all font files in case they are not used immediately
+if ('fonts' in document) {
+  const inter400 = new FontFace(
+    'Inter',
+    'url(/assets/fonts/Inter-Regular-subset.woff2)',
+    { weight: 400 }
+  )
+  const inter700 = new FontFace(
+    'Inter',
+    'url(/assets/fonts/Inter-Bold-subset.woff2)',
+    { weight: 700 }
+  )
+  const inter400italic = new FontFace(
+    'Inter',
+    'url(/assets/fonts/Inter-Italic-subset.woff2)',
+    { weight: 400, style: 'italic' }
+  )
+  const inter800 = new FontFace(
+    'Inter',
+    'url(/assets/fonts/Inter-ExtraBold-subset.woff2)',
+    { weight: 800 }
+  )
+
+  Promise.all([
+    inter400.load(),
+    inter400italic.load(),
+    inter700.load(),
+    inter800.load()
+  ])
+    .then((fonts) => {
+      if (__DEV__) {
+        fonts.forEach((font) => {
+          console.log(
+            `FontFace loaded: ${font.family} ${font.weight} ${font.style}`,
+            font
+          )
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(`Error while loading font files: ${error.message}`)
+    })
+}
