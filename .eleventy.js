bcb669ec047980f76307b9e2040818dff29a5d2a
Jeremias Menichelli <jmenichelli@gmail.com>
2023-05-23 11:56:52 +0200

    Revert "chore: Configure headers and redirects for Cloudflare Pages (#121)" (#134)
    This reverts commit a56a4122d0ac994461f48410d0015638f3c5edb5.

--- .eleventy.js
+++ .eleventy.js
@@ -38,8 +38,6 @@ module.exports = function (eleventyConfig) {
   /* COPY */
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy('robots.txt');
-  eleventyConfig.addPassthroughCopy('_headers');
-  eleventyConfig.addPassthroughCopy('_redirects');
 
   /* HTML */
   eleventyConfig.addTransform('htmlmin', htmlmin);
